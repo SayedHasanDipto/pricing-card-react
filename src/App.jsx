caa153e0e2715c634764669c6ba380e5443e5fb1
Sayed Hasan Dipto <sayedhasandipto@gmail.com>
2026-03-25 04:05:17 +0600

    Reciving json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Suspense } from 'react';
 import './App.css'
+import PricingOption from './component/pricingOptions/PricingOption'
+
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
+
 
 function App() {
   return (
     <>
-
+      <main>
+        <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>
+        }>
+          <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
+      </main>
     </>
   )
 }
